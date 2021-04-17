f97e4f5dd1575b093fa67637eb18c808867f38bb
richiegander <82710720+richiegander@users.noreply.github.com>
2021-04-17 14:33:19 +0100

    Update app.js
    2 lines of code

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Susan!'));
+app.get('/', (req, res) => res.send('The first end to end pipeline commit with manual review approval stage!'));
+
+app.get('/', (req, res) => res.send('Rudimentary'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
