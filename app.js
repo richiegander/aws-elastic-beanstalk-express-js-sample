731fe4b8d2d8145f31a40ff2389b025689e2d49c
richiegander <82710720+richiegander@users.noreply.github.com>
2021-04-17 14:26:36 +0100

    Update app.js
    change to susan

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello everyone!'));
+app.get('/', (req, res) => res.send('Hello Susan!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
